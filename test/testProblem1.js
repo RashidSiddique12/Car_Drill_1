dac26327de63d5c0239f032a35bc567697dec575
Rashid <stylorashid786@gmail.com>
2023-11-09 16:07:03 +0530

    Problem1

---
+++ test/testProblem1.js
@@ -0,0 +1,7 @@
+// const {inventory} = require('../inventoryData');
+const {carDetails} = require('../problem1');
+const carId = 33
+const carData = carDetails(carId);
+
+// "Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
+console.log(`Car 33 is a ${carData.car_year} ${carData.car_make} ${carData.car_model}` )
